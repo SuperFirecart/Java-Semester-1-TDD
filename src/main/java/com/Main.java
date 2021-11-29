e6eb504d996e5a5d924f780f79acb89c0d1b7df1
Conor Claret <78763959+SuperFirecart@users.noreply.github.com>
2021-11-29 13:39:24 +0000

    Files created for project
    Files created for creating a JavaFX application, along with Maven Build and Testing tools

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,9 @@
+package main.java.com;
+import javafx.stage.Stage;
+
+public class Main {
+    public static void main(String[] args){
+        Stage stage = new Stage() ;
+        stage.show();
+    }
+}
---
+++ src/test/java/com/TestUsername.java
@@ -0,0 +1,16 @@
+package com;
+
+
+import org.junit.Test;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
+public class TestUsername {
+    @Test
+    public void testing() throws IOException {
+        System.out.println("yeet");
+    }
+}
